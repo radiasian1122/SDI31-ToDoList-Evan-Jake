6f26f42e2f9d9356a8f236650f0433208efa5884
jake <jake.glee1122@gmail.com>
2025-07-09 13:47:26 -0700

    Co-authored-by: Aesiren <Aesiren@users.noreply.github.com>1

--- functions.js
+++ functions.js
@@ -1,41 +1,74 @@
 /*TODO
-define variables
-accept input
-create li utilizing the accepted input
+reset input form
+configure remove completed button
+move completed items to completed items list
 
-PSUDO CODE:
-var taskName
-
-function onClick (receive a string){
-set taskName to received string
-create item for list item as:
-'<li><checkbox>taskName</li>
-
-get content of unordered list div
-append div with created item
-}
 
 */
 
 
 let btn = document.querySelector('#addTask');
-//let textBox = document.querySelector('#textInput').value;
+let removeBtn = document.querySelector('#removeTasks');
 let unorderedList = document.querySelector('#currentList');
+let completedList = document.querySelector('#completedList');
+let moveItem;
 
 
 btn.addEventListener("click", ()=>{
   let textBox = document.querySelector('#textInput').value;
   addTask(textBox);
-}
+  resetForm();
+})
+
+removeBtn.addEventListener("click", ()=>{
+  //console.log('remove task button clicked');
+  removeTask();
+})
   
-)
+
 
 function addTask(newString){
   let listItem = document.createElement('li');
   console.log(newString);
   
-  listItem.innerHTML = "<input type='checkbox'/><label>"+newString+"</label>";
+  listItem.innerHTML = "<label class='strikethrough'><input type='checkbox'/>"+newString+"</label>";
   
   unorderedList.appendChild(listItem);
+  
 }
 
+function resetForm(){
+  document.querySelector('#textInput').value = "";
+}
+
+function moveCompleted(moveItem){
+
+  completedList.append(moveItem);
+}
+
+function removeTask() {
+  //get all elements with strikethorugh class
+  //check if element has value of checked variablename.checked
+  //remove that value
+    let removeItem = document.getElementsByClassName('strikethrough');
+    //console.log(removeItem.length);
+    let x = 0;
+    while (x < removeItem.length){
+      //console.log("on step ", x, "removeitem length: ", removeItem.length)
+      let isChecked = removeItem[x].childNodes[0].checked;
+      //console.log("removing: " + x);
+      if (isChecked){
+        moveItem = unorderedList.children[x];
+        unorderedList.removeChild(unorderedList.children[x]);
+
+        if (x > 0){
+          x --;
+        }
+        moveCompleted(moveItem);
+      } else {
+        x++;
+      }
+      
+    }
+
+}
--- readme.md
+++ readme.md
@@ -1,10 +1,18 @@
 
 [X] Users should be able to view all of the to-do tasks
 [X] Users should be able to add a to-do to the list of to-do tasks
-[ ] Users should be able to mark a task as completed by crossing it out on click
-[ ] Users should be able to remove to-do tasks by removing those elements from the DOM
-[ ] The page should be aesthetically pleasing (aka be styled)
+[X] Users should be able to mark a task as completed by crossing it out on click
+[X] Users should be able to remove to-do tasks by removing those elements from the DOM
+[X] The page should be aesthetically pleasing (aka be styled)
 
 [ ] Add some instructions to your README.md file around what your application is, how to run it, and how to use it.
 [ ] Make the list persist through a refresh of the page by using local storage to store to-do's
-[ ] Archive completed tasks in a viewable location on screen
+[X] Archive completed tasks in a viewable location on screen
+
+Instructions:
+1. Enter task name in the "Enter Task Name" box
+2. Click the "Add Task" button
+3. When completed with a task, click the checkbox next to the task name
+4. Click "Remove Completed" when all marked tasks can be archived
+5. Enjoy your day!
+
